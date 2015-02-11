9738d7e46b1f2f99334b32cadcc448024c4aee1b
fitnr <fitnr@fakeisthenewreal>
2015-02-11 11:43:16 -0500

    add basic-basic tests

---
+++ tests/fleetmonger_tests.py
@@ -0,0 +1,17 @@
+import sys
+from fleetmonger import Fleetmonger
+import unittest
+
+class FleetMongerTestCase(unittest.TestCase):
+
+    def setUp(self):
+
+        self.f = Fleetmonger(user, key)
+
+    def test_fleet(self):
+        fleet = self.f.myfleet()
+        assert fleet
+        assert len(fleet) > 0
+
+if __name__ == '__main__':
+    unittest.main()
